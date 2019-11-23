2989d739d0eeca95b6653ab0fb072c63dd309993
Raviraj Shettigar <ravirajs@citiustech.com>
2019-11-24 00:45:41 +0530

    initial commit

---
+++ src/main/java/com/citiustech/sparrow/sparrowweb/SparrowWebApplication.java
@@ -0,0 +1,13 @@
+package com.citiustech.sparrow.sparrowweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparrowWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparrowWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/citiustech/sparrow/sparrowweb/SparrowWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citiustech.sparrow.sparrowweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparrowWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
