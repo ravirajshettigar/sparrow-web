ed489205a70f55bb2eb31f418bcaac977c359411
Saleem Rahman <SaleemR@citiustech.com>
2021-11-02 19:51:15 +0530

    Added Hello World

--- src/main/java/com/citiustech/sparrow/sparrowweb/SparrowWebApplication.java
+++ src/main/java/com/citiustech/sparrow/sparrowweb/SparrowWebApplication.java
@@ -8,6 +8,7 @@ public class SparrowWebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SparrowWebApplication.class, args);
+                System.out.println("Hello World");
 	}
 
 }
