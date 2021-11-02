43e28655a7abbe3b34df32246c3b350dbe45b87f
Saleem Rahman <SaleemR@citiustech.com>
2021-11-02 20:03:35 +0530

    Changed to how are u

--- src/main/java/com/citiustech/sparrow/sparrowweb/SparrowWebApplication.java
+++ src/main/java/com/citiustech/sparrow/sparrowweb/SparrowWebApplication.java
@@ -8,7 +8,7 @@ public class SparrowWebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SparrowWebApplication.class, args);
-                System.out.println("Hello World");
+                System.out.println("How are you");
 	}
 
 }
